2cd60b7e1b0452e6b8c10e3bb98a8633d449fe78
Jayesh Shinde <jayesh.v.shinde1251@gmail.com>
2024-12-12 15:47:33 +0530

    Created Netflix GPT

---
+++ netflix-gpt/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl text-rose-600 font-bold'>Hello Vite!</h1>
+    </>
+  )
+}
+
+export default App
